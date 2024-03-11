054b247cdabc0c013bcb8b99654bad85fde4216f
egormuhaev <egormuhaev@gmail.com>
2024-03-12 00:10:35 +0300

    fix node modules

--- builder_prototype/dist/SetupControllers/Controllers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Controllers = void 0;
-class Controllers {
-    constructor() { }
-}
-exports.Controllers = Controllers;
-//# sourceMappingURL=Controllers.js.map
--- builder_prototype/dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SetupProject_1 = require("./SetupProject/SetupProject");
-const pr = new SetupProject_1.Project();
-const targetPath = pr.setupProject('project_1');
-console.log(targetPath);
-//# sourceMappingURL=main.js.map
